75e5c96b71894298ff2b043cd65167c1ca88d098
ulivz <472590061@qq.com>
2017-09-24 02:19:42 +0800

    [Release] v1.0.6

--- app/main.js
+++ app/main.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import simple from '../examples/simple.vue'
-
-// import VCodemirror from '../index'
-//
-// Vue.use(VCodemirror)
 
 new Vue({
   el: '#app',
