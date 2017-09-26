d130fe2662336f83d1f3ebfdb6851bcc17977756
ulivz <472590061@qq.com>
2017-09-26 23:41:45 +0800

    add eslint and fix eslint error

--- src/util.js
+++ src/util.js
@@ -5,38 +5,37 @@
  * @returns {*}
  */
 export function findMode(CodeMirror, mode) {
+  let lang
   // mode string config
   if (typeof mode == 'string') {
-    var lang = CodeMirror.findModeByMIME(mode)
-    console.log(lang)
+    lang = CodeMirror.findModeByMIME(mode)
     mode = !lang ? lang : lang.mode
-    console.log(mode)
 
     // mode object config
   } else if (typeof mode == 'object') {
     if (mode.name) {
-      var lang = CodeMirror.findModeByName(mode.name)
+      lang = CodeMirror.findModeByName(mode.name)
       if (lang) {
         mode = lang.mode
       } else {
         mode = null
       }
     } else if (mode.ext) {
-      var lang = CodeMirror.findModeByExtension(mode.ext)
+      lang = CodeMirror.findModeByExtension(mode.ext)
       if (lang) {
         mode = lang.mode
       } else {
         mode = null
       }
     } else if (mode.mime) {
-      var lang = CodeMirror.findModeByMIME(mode.mime)
+      lang = CodeMirror.findModeByMIME(mode.mime)
       if (lang) {
         mode = lang.mode
       } else {
         mode = null
       }
     } else if (mode.filename) {
-      var lang = CodeMirror.findModeByFileName(mode.filename)
+      lang = CodeMirror.findModeByFileName(mode.filename)
       if (lang) {
         mode = lang.mode
       } else {
@@ -44,7 +43,6 @@ export function findMode(CodeMirror, mode) {
       }
     }
   }
-  console.log(mode)
   return mode
 }
 
